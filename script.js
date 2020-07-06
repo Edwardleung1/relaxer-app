fdc6aebb11e625cb69140e501a49335ac0e30486
Edward Leung <edward_leung_12@hotmail.com>
2020-07-06 21:32:07 +0100

    finished breathe app

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-const container = document.querySelector('.container')
-const text = document.querySelector('#text')
+const container = document.getElementById('container');
+const text = document.getElementById('text');
 
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
 
+breathAnimation();
 
-const totalTime = 7500 // 7 seconds
-const breatheTime = (totalTime / 5) * 2 // this gives me 3 seconds
-const holdTime = totalTime / 5 // 1.5 seconds
+function breathAnimation() {
+  text.innerText = 'Breathe In!';
+  container.className = 'container grow';
 
-breatheAnimation()
-
-function breatheAnimation() {
-    text.innerHTML = 'Breath in!'
-    container.className = 'container grow'
+  setTimeout(() => {
+    text.innerText = 'Hold';
 
     setTimeout(() => {
-        text.innerText = 'Hold'
-
-        setTimeout(() => {
-            text.innerText = 'Breathe Out!'
-            container.className = 'container shrink'
-        }, holdTime)
-    }, breatheTime)
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
 }
 
-setInterval(breatheAnimation, totalTime)
+setInterval(breathAnimation, totalTime);
