fe73b19f1bb7c952a46e1b887c938cf213f6b93d
Edward Leung <edward_leung_12@hotmail.com>
2020-07-06 20:57:02 +0100

    created variables for the relaxer-app

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container')
+const text = document.querySelector('#text')
+
+
+
+const totalTime = 7500
+const breatheTime = (totalTime / 5) * 2 // this gives me 3 seconds
