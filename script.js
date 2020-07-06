217eba219afbc1347b5934b0d032b49555e35a67
Edward Leung <edward_leung_12@hotmail.com>
2020-07-06 21:05:27 +0100

    now time to add style.css

--- script.js
+++ script.js
@@ -3,5 +3,24 @@ const text = document.querySelector('#text')
 
 
 
-const totalTime = 7500
+const totalTime = 7500 // 7 seconds
 const breatheTime = (totalTime / 5) * 2 // this gives me 3 seconds
+const holdTime = totalTime / 5 // 1.5 seconds
+
+breatheAnimation()
+
+function breatheAnimation() {
+    text.innerHTML = 'Breath in!'
+    container.className = 'container grow'
+
+    setTimeout(() => {
+        text.innerText = 'Hold'
+
+        setTimeout(() => {
+            text.innerText = 'Breathe Out!'
+            container.className = 'container shrink'
+        }, holdTime)
+    }, breatheTime)
+}
+
+setInterval(breatheAnimation, totalTime)
